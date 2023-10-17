e2fbb59b2b1a5d997db801f9cee98185b6822cc3
Artem Bortnikov <26360954+brongineer@users.noreply.github.com>
2023-10-17 14:12:27 +0300

    command tests
    Signed-off-by: Artem Bortnikov <brongineer747@gmail.com>

--- command_test.go
+++ command_test.go
@@ -1,8 +1,96 @@
 package gocli
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+type CommandConfig struct {
+	Field1 string
+	Field2 int
+	Field3 bool
+}
+
+func (t *CommandConfig) LoadFromFiles(_ []string) error {
+	return nil
+}
+
+func (t *CommandConfig) LoadFromEnv(_ string) error {
+	return nil
+}
+
+func (t *CommandConfig) LoadFromFlags(_ FlagSet) error {
+	return nil
+}
+
+func TestNewCommand(t *testing.T) {
+	t.Parallel()
+	cmd := NewCommand("test")
+	assert.NotEmpty(t, cmd)
+	assert.Equal(t, "test", cmd.Name)
+	assert.NotEmpty(t, cmd.FlagSet)
+}
+
+func TestCommand_WithFlag(t *testing.T) {
+	t.Parallel()
+	flag := NewFlag[string]("test", "")
+	cmd := NewCommand("test").WithFlag(flag)
+	actual := cmd.Flag("test")
+	assert.NotNil(t, actual)
+	assert.True(t, reflect.DeepEqual(flag, actual))
+	assert.Equal(t, len(cmd.Flags()), 1)
+}
+
+func TestCommand_WithSubcommand(t *testing.T) {
+	t.Parallel()
+	cmd := NewCommand("test")
+	assert.Nil(t, cmd.Subcommands)
+	subcmd := NewCommand("subtest")
+	cmd = cmd.WithSubcommand(subcmd)
+	assert.NotNil(t, cmd.Subcommands)
+}
+
+func TestCommand_WithRunFunc(t *testing.T) {
+	t.Parallel()
+	cmd := NewCommand("test").
+		WithRunFunc(func(command Command) {
+
+		})
+	assert.NotNil(t, cmd.Run)
+}
+
+func TestCommand_WithRunEFunc(t *testing.T) {
+	t.Parallel()
+	cmd := NewCommand("test").
+		WithRunEFunc(func(command Command) error {
+			return nil
+		})
+	assert.NotNil(t, cmd.RunE)
+}
+
+func TestCommand_WithConfig(t *testing.T) {
+	t.Parallel()
+	cfg := &CommandConfig{}
+	cmd := NewCommand("test").
+		WithConfig(cfg)
+	assert.NotNil(t, cmd.Config)
+}
+
+func TestCommand_Execute(t *testing.T) {
+	var err error
+	t.Parallel()
+	cmd := NewCommand("test")
+	err = cmd.Execute()
+	assert.Error(t, err)
+	cmd = cmd.WithRunFunc(func(command Command) {})
+	err = cmd.Execute()
+	assert.NoError(t, err)
+	cmd = cmd.WithRunEFunc(func(command Command) error { return nil })
+	err = cmd.Execute()
+	assert.NoError(t, err)
+}
 
 func BenchmarkNewCommand(b *testing.B) {
 	b.ResetTimer()
