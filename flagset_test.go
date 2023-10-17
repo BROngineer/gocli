1a7279d6f6f364e95482cd43424e859840982141
Artem Bortnikov <26360954+brongineer@users.noreply.github.com>
2023-10-17 12:55:36 +0300

    flagset tests
    Signed-off-by: Artem Bortnikov <brongineer747@gmail.com>

--- flagset_test.go
+++ flagset_test.go
@@ -1,8 +1,68 @@
 package gocli
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestNewFlagSet(t *testing.T) {
+	t.Parallel()
+	fs := NewFlagSet()
+	assert.NotEmpty(t, fs)
+	assert.NotNil(t, fs.Flags)
+}
+
+func TestFlagSet_AddFlag(t *testing.T) {
+	t.Parallel()
+	fs := NewFlagSet()
+	expected := NewFlag[string]("test", "")
+	fs.AddFlag(NewFlag[string]("test", ""))
+	actual := fs.Flag("test")
+	assert.True(t, reflect.DeepEqual(expected, actual))
+}
+
+func TestFlagSet_Merge(t *testing.T) {
+	t.Parallel()
+	fs1 := NewFlagSet()
+	fs2 := NewFlagSet()
+	fs1.AddFlag(NewFlag[string]("test", ""))
+	fs2.Merge(&fs1)
+	expected := fs1.Flag("test")
+	actual := fs2.Flag("test")
+	assert.NotNil(t, actual)
+	assert.True(t, reflect.DeepEqual(expected, actual))
+}
+
+func TestFlagSet_Flag(t *testing.T) {
+	var actual Flag
+	t.Parallel()
+	fs := NewFlagSet()
+	f := NewFlag[string]("test", "").WithShorthand("t")
+	fs.AddFlag(f)
+	actual = fs.Flag("test")
+	assert.NotNil(t, actual)
+	assert.True(t, reflect.DeepEqual(f, actual))
+	actual = fs.Flag("t")
+	assert.NotNil(t, actual)
+	assert.True(t, reflect.DeepEqual(f, actual))
+	actual = fs.Flag("testing")
+	assert.Nil(t, actual)
+}
+
+func TestGetValue(t *testing.T) {
+	t.Parallel()
+	fs := NewFlagSet()
+	fs.AddFlag(NewFlag[string]("test", "").WithDefault("sample"))
+	v, err := GetValue[string](fs, "test")
+	assert.NoError(t, err)
+	assert.Equal(t, "sample", *v)
+	_, err = GetValue[string](fs, "testing")
+	assert.Error(t, err)
+	_, err = GetValue[int](fs, "test")
+	assert.Error(t, err)
+}
 
 func BenchmarkNewFlagSet(b *testing.B) {
 	b.ResetTimer()
